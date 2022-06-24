a2d7eb68aa4d85a04258fca06fdd7ca9e0484fbb
Mischka Booley <mischka@gearbox.com.au>
2022-06-24 19:36:18 +1000

    commit tree 1f4d9865e572b858e3747fe665d013944fe6dc33

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const userEndpoint = "http://localhost:3000/users";
+
+function submitData(fullname, emailAddress) {
+  return fetch(userEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: fullname,
+      email: emailAddress
+    })
+  })
+    .then(resp => resp.json())
+    .then(json => {
+      let id = document.createElement("p");
+      id.innerText = json.id;
+      document.body.appendChild(id);
+      console.log(json.id)
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+};
+
+submitData("Bodine", "bthompson93@outlook.com.au");
